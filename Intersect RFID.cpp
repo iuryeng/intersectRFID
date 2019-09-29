d949bd60ee90c4ccfe022bf0525c708b22b66106
Iury Coelho <38250160+iuryeng@users.noreply.github.com>
2019-09-28 22:39:55 -0300

    Add files via upload

---
+++ Intersect RFID.cpp
@@ -0,0 +1,18 @@
+// Intersect RFID.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace IntersectRFID;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
